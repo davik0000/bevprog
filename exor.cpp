f1ffba87000286c6c5fb006cc1ad7152eb391823
davik0000 <43435085+davik0000@users.noreply.github.com>
2018-09-27 10:24:07 +0200

    Add files via upload

---
+++ exor.cpp
@@ -0,0 +1,19 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+
+cout<<"Adja meg a 2 számot\n";
+  int elso;
+  int masodik;
+
+cin>>elso>>masodik;
+  
+	elso = elso^masodik;
+	masodik=elso^masodik;
+	elso=elso^masodik;
+
+ cout<<"A muvelet utan :"<<elso<<" "<<masodik<<"\n";
+return 0;
+}
+
